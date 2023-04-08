87ead6485edf660066317715a8b6cd1710cfb82a
liebertuslin <codelinkursus@gmail.com>
2023-04-09 06:41:11 +0700

    Ini adalah block kode yang sudah berhasil di revisi

---
+++ tests/Feature/FooBarServiceProviderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Data\Foo;
+use App\Data\Bar;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FooBarServiceProviderTest extends TestCase
+{
+    public function testServiceProvider()
+    {
+        $foo1 = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertSame($foo1, $foo2);
+
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertSame($bar1, $bar2);
+
+        self::assertSame($foo1, $bar1->foo);
+        self::assertSame($foo2, $bar2->foo);
+    }
+}
+
+// ini adalah bagian dari provider yang dapat kita jalankan jika kita sudah membuat objek baru dari sebuah kelas test unit
